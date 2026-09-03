e82d17f55316a34b0a2fb2e35b58d7d70a9ea546
TapiaXimena <x.tapia92@outlook.com>
2026-09-03 19:22:48 -0300

    Se completa clase03 JS

---
+++ Javascript/Clase03/e-commerce/client/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "../media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "../media/leche.png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img: "../media/pollo.png",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 950,
+        quanty: 1,
+        img: "../media/mayonesa.png",
+    },
+      {
+        id: 5,
+        productName: "enlatados",
+        price: 50,
+        quanty: 1,
+        img: "../media/enlatados.png",
+    },
+       {
+        id: 5,
+        productName: "varios",
+        price: 9950,
+        quanty: 1,
+        img: "../media/varios.png",
+    },
+]
