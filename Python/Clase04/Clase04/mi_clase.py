f250e3f686df9d6142fa7b5d25e92b17e48bf35f
Franco279 <frnccala@gmail.com>
2026-09-08 13:01:27 -0300

    Add files via upload

---
+++ Python/Clase04/Clase04/mi_clase.py
@@ -0,0 +1,22 @@
+
+
+class Miclase:
+    """
+    Esta es una documentacion una documentacion de esta clase,
+     es solo para saber que aqui es Miclase
+    """
+    def __init__(self):
+        """
+        Aqui esta el metodo dunder init
+        Este metodo es el de incio
+        para esta clase
+        """
+    def mi_metodo(self, param1, param2):
+            """
+            Este es el metodo creado por mi que recibe parametros
+            detecto los parametros automaticamente
+            :param self: Este es el parametro numero 1
+            :param param1:  Este es el parametro numero 2
+            :param param2: Este es el valor del retorno
+            :return:
+            """
